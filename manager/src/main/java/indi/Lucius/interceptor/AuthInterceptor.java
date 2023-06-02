14464ab6c37ebc45fbd27ae5c6cb1d396b8325d5
LuciusPan <83498111+Lucius-Pan@users.noreply.github.com>
2023-06-02 14:12:30 +0800

    小幅度修改

--- manager/src/main/java/indi/Lucius/interceptor/AuthInterceptor.java
+++ manager/src/main/java/indi/Lucius/interceptor/AuthInterceptor.java
@@ -16,7 +16,12 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("收到了请求"+request.getRequestURI());
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        if (request.getRequestURI().equals("/manager/userLogin") || request.getRequestURI().equals("/manager/insertEmp") || request.getRequestURI().equals("/manager/selectAllEmp") || request.getRequestURI().equals("/updateEmp")) {
+            return true;
+        } else {
+            return false;
+        }
+//        return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
